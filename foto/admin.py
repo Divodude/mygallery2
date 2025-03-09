31e92aad51b74f161af225854b20bceea59f55ce
Divyansh <155663331+Divodude@users.noreply.github.com>
2025-03-09 08:40:45 +0530

    Delete foto directory

--- foto/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from foto.models import dataset,profile,user_images,clubdb
-
-# Register your modeils here.
-
-admin.site.register(dataset)
-admin.site.register(profile)
-admin.site.register(user_images)
-admin.site.register(clubdb)
--- foto/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path,include
-from foto.api import views
-
-urlpatterns = [
-    path("data/",views.api)
-]
